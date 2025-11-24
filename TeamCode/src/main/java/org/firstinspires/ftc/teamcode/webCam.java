4cd1ffc408fcab1e76e7a28ea300b332d90f8944
Luis Chen <luischen904@gmail.com>
2025-11-24 00:07:15 -0600

    camera

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/webCam.java
@@ -0,0 +1,64 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.util.Size;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class webCam {
+    private AprilTagProcessor aprilTagProcessor;
+    private VisionPortal visionPortal;
+    private List<AprilTagDetection> detectedTags = new ArrayList<>();
+    private Telemetry telemetry;
+
+    public void init(HardwareMap hwMap, Telemetry telemetry) {
+        this.telemetry = telemetry;
+        
+//        aprilTagProcessor = new AprilTagProcessor.Builder();
+//        .setDrawTagOutline(true)
+//                .setDrawAxes(true)
+//                .setDrawCubeProjection(true)
+//                .setOutputUnits(DistanceUnit.CM, AngleUnit.DEGREES)
+//                .build();
+
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+        builder.setCamera(hwMap.get(WebcamName.class, "Webcam 1"));
+        builder.setCameraResolution(new Size(640, 480));
+        builder.addProcessor(aprilTagProcessor);
+
+        visionPortal = builder.build();
+    }
+
+    public void update() {
+        detectedTags = aprilTagProcessor.getDetections();
+    }
+
+    public List<AprilTagDetection> getDetectedTags() {
+        return detectedTags;
+    }
+
+    public AprilTagDetection getTagBySpecificId(int id) {
+        for (AprilTagDetection detection : detectedTags) {
+            if (detection.id == id) {
+                return detection;
+            }
+        }
+        return null;
+    }
+
+    public void stop() {
+        if (visionPortal != null) {
+            visionPortal.close();
+        }
+    }
+}
