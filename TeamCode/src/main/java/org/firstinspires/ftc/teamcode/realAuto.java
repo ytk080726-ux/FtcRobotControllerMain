79f965802cf9f67ee8474ccc491267ee658b493d
ytk080726-ux <ytk080726@gmail.com>
2025-12-03 23:06:08 -0600

    test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/realAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/realAuto.java
@@ -18,50 +18,27 @@ public class realAuto extends LinearOpMode {
     DcMotor frontRightMotor;
     DcMotor frontLeftMotor;
     DcMotor backLeftMotor;
-    //DcMotor shootwheel;
-    //Servo artifactstopper;
-//    ColorSensor color1;
-//    DistanceSensor distance1;
     BNO055IMU imu;
 
-    int forward, turn, mode, maxDrivePower, strafe;
+    int turn, mode, maxDrivePower, strafe;
     long duration;
-    //VisionPortal myVisionPortal;
     boolean isShooting;
-    double shootPower;
+    double shootPower, forward;
 
-    // Describe this function...
     public void inititalSetup(){
-        // Put initialization blocks here
         frontRightMotor.setDirection(DcMotor.Direction.REVERSE);
         backRightMotor.setDirection(DcMotor.Direction.FORWARD);
 
         frontLeftMotor.setDirection(DcMotor.Direction.FORWARD);
         backLeftMotor.setDirection(DcMotor.Direction.FORWARD);
 
-
         isShooting = false;
-        // Holds back artifacts until we start shooting
-        //artifactstopper.setPosition(0.2);
     }
 
-    // Describe this function...
-//    public void initializeVisionPortal(){
-//        VisionPortal.Builder builder = new VisionPortal.Builder();
-//        myVisionPortal = builder.build();
-//    }
-
-    // Describe this function...
     public void pickMode(){
         autoDrive();
     }
 
-    // Describe this function...
-    public void autoDrive(){
-        driveToGoal();
-    }
-
-    // Describe this function...
     public void driveToGoal(){
         forward = 1;
         processInputsAndSleep(800);
@@ -70,50 +47,45 @@ public class realAuto extends LinearOpMode {
         sleep(50);
     }
 
-    // Describe this function...
+    public void autoDrive(){
+        driveToGoal();
+    }
+
     public void processInputsAndSleep(long duration){
-        // This helper function makes the code a bit cleaner
         processDriveInputs();
 
         sleep(duration);
         forward = 0;
         turn = 0;
         strafe = 0;
-        // Stop all movement after sleep
         processDriveInputs();
     }
 
-    // Describe this function...
     public void processDriveInputs(){
         turn = turn * maxDrivePower;
-        forward = forward * maxDrivePower;
+        forward = forward * 0.5;
         strafe = strafe * maxDrivePower;
-        // Combine inputs to create drive and turn (or both!)
 
-        backLeftMotor.setPower(0.5);
-        frontLeftMotor.setPower(0.5);
-        backRightMotor.setPower(0.5);
-        frontRightMotor.setPower(0.5);
-        sleep(50);
+        backLeftMotor.setPower(forward);
+        frontLeftMotor.setPower(forward);
+        backRightMotor.setPower(forward);
+        frontRightMotor.setPower(forward);
+
+        forward = 0;
+        sleep(300);
     }
 
     @Override
   public void runOpMode() {
-       // shootwheel = hardwareMap.get(DcMotor.class, "shootwheel");
          backLeftMotor = hardwareMap.get(DcMotor.class, "backLeftMotor");
          backRightMotor = hardwareMap.get(DcMotor.class, "backRightMotor");
 
          frontLeftMotor = hardwareMap.get(DcMotor.class, "frontLeftMotor");
          frontRightMotor = hardwareMap.get(DcMotor.class, "frontRightMotor");
-        //artifactstopper = hardwareMap.get(Servo.class, "artifactstopper");
-        //color1 = hardwareMap.get(ColorSensor.class, "color1");
-        //distance1 = hardwareMap.get(DistanceSensor.class, "distance1");
         imu = hardwareMap.get(BNO055IMU.class, "imu");
         inititalSetup();
-        //initializeVisionPortal();
         shootPower = 0.8;
         maxDrivePower = 1;
-        // mode 0 = keyboard, 1 = gamepad, 2 = autonomous
         mode = 2;
         waitForStart();
         pickMode();
