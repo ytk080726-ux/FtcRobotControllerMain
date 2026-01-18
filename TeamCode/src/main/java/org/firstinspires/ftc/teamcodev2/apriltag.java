fba3fdfa9bd1d36e408d2018360a44769ee7a098
ytk080726-ux <ytk080726@gmail.com>
2026-01-18 14:29:32 -0600

    fornow

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcodev2/apriltag.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcodev2/apriltag.java
@@ -5,13 +5,18 @@ import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.tel
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
+import com.qualcomm.hardware.limelightvision.LLStatus;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+
+import java.util.List;
 
 
 public class apriltag {
@@ -26,15 +31,16 @@ public class apriltag {
         limelight = hw.get(Limelight3A.class, "limelight");
         limelight.pipelineSwitch(9);
         imu = hw.get(IMU.class, "imu");
+        limelight.setPollRateHz(100); // This sets how often we ask Limelight for data (100 times per second)
         RevHubOrientationOnRobot revHubOrientationOnRobot = new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
                 RevHubOrientationOnRobot.UsbFacingDirection.UP);
         imu.initialize(new IMU.Parameters(revHubOrientationOnRobot));
     }
-    public void start()
+    public void starting()
     {
         limelight.start();
     }
-    public void stop()
+    public void stopping()
     {
         limelight.stop();
     }
@@ -43,7 +49,6 @@ public class apriltag {
         YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
         limelight.updateRobotOrientation(orientation.getYaw());
         LLResult llresult = limelight.getLatestResult();
-
         if(llresult != null && llresult.isValid()) {
             Pose3D botPose = llresult.getBotpose_MT2();
             telemetry.addData("Tx", llresult.getTx());
@@ -52,24 +57,64 @@ public class apriltag {
             telemetry.addData("bot pose",botPose.toString());
             telemetry.addData("yaw",botPose.getOrientation().getYaw(AngleUnit.DEGREES));
         }
-    }
-    public double getTa() {
-        update();
-        return Ta;
-    }
-    public double getTx() {
-        update();
-        return Tx;
-    }
-    public double getTy() {
-        update();
-        return Ty;
+        else
+        {
+            telemetry.addData("lielight","no");
+        }
     }
 
+
     public double getDistance() {
+        //change bot pose to flucial not mt2 turn the degree to angle
         update();
         double up=75;
-        double scale = (up-27.7)/(Math.tan(20+getTy()));
+        double scale= 0;
+        LLResult llresult = limelight.getLatestResult();
+        if(llresult != null && llresult.isValid()) {
+            List<LLResultTypes.FiducialResult> fiducials = llresult.getFiducialResults();
+            Pose3D botPose = llresult.getBotpose_MT2();
+            double first = (up - 27.7) / (Math.tan(20 + llresult.getTy()));
+            double second=Math.sqrt(Math.pow(botPose.getPosition().x,2)+Math.pow(botPose.getPosition().y,2));
+            scale=(first+second)/2;
+        }
         return (scale);
+    }
+    //m should be 0.4m
+    public boolean correct(double m)
+    {
+        update();
+        double up=75;
+        LLResult llresult = limelight.getLatestResult();
+        if(llresult != null && llresult.isValid()&&getDistance()!=0) {
+            Pose3D botPose = llresult.getBotpose_MT2();
+            double degree1,degree2,degree3,degree4,tl;
+            degree1=llresult.getTx();
+            degree2=Math.acos(botPose.getPosition().y/getDistance());
+            degree3=90+degree2;
+            tl=Math.sqrt(Math.pow(getDistance(),2)+Math.pow(m,2)-2*m*getDistance()*Math.cos(degree3));
+            degree4=Math.asin(m*Math.sin(degree3)/tl);
+            return (degree1<degree4);
+        }
+        return false;
+    }
+    public double realpower(double m)
+    {
+        //change bot pose to flucial not mt2 turn the degree to angle
+        LLResult llresult = limelight.getLatestResult();
+        Pose3D botPose = llresult.getBotpose_MT2();
+        double power= 0;
+        if(correct(m))
+        {
+            double degree1,degree2,degree3,degree4,degree5,k,realdistahce;
+            degree1=llresult.getTx();
+            degree2=Math.acos(botPose.getPosition().y/getDistance());
+            degree3=90+degree2;
+            realdistahce=Math.sqrt(Math.pow(getDistance(),2)+Math.pow(m,2)-2*m*getDistance()*Math.cos(degree3));
+            degree4=(90-degree2-degree1);
+            degree5=180-degree4;
+            k=Math.sin(degree5)*m/Math.cos(degree4);
+            power=k+realdistahce;
+        }
+        return power;
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcodev2/drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcodev2/drive.java
@@ -1,5 +1,10 @@
 package org.firstinspires.ftc.teamcodev2;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
+
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -7,11 +12,22 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+
+import java.util.List;
 
 @TeleOp(name = "(new) driveV2")
 public class drive extends LinearOpMode {
+    private Limelight3A limelight;
+    IMU imu;
     @Override
     public void runOpMode() throws InterruptedException {
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight.pipelineSwitch(9);
+        limelight.setPollRateHz(100); // This sets how often we ask Limelight for data (100 times per second)
+        RevHubOrientationOnRobot revHubOrientationOnRobot = new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
+                RevHubOrientationOnRobot.UsbFacingDirection.UP);
         // Declare our motors
         // Make sure your ID's match your configuration
         DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeft");
@@ -27,13 +43,14 @@ public class drive extends LinearOpMode {
         backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
         // Retrieve the IMU from the hardware map
-        IMU imu = hardwareMap.get(IMU.class, "imu");
+        imu = hardwareMap.get(IMU.class, "imu");
         // Adjust the orientation parameters to match your robot
         IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
                 RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
                 RevHubOrientationOnRobot.UsbFacingDirection.UP));
         // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
         imu.initialize(parameters);
+        limelight.start();
 
         waitForStart();
 
@@ -50,9 +67,6 @@ public class drive extends LinearOpMode {
 
         launching launch = new launching();
         launch.init(hardwareMap);
-
-        apriltag april = new apriltag();
-        april.init(hardwareMap);
 
         lifting lift = new lifting();
         lift.init(hardwareMap);
@@ -103,7 +117,6 @@ public class drive extends LinearOpMode {
             if(gamepad1.xWasPressed()) {
                 transfer.start();
             }
-
             blocker.stopping(gamepad1.b);
 
             if (gamepad1.rightBumperWasPressed()) {
@@ -113,7 +126,9 @@ public class drive extends LinearOpMode {
             telemetry.addData("mode",launch.state());
             telemetry.update();
             telemetry.addData("Speed",launch.showRPM());
-            telemetry.addData("Distance: ", april.getDistance());
+            updating();
+            telemetry.addData("Distance: ",getDistance());
+
 
             if (gamepad1.dpadUpWasPressed()) {
                 lift.up();
@@ -136,5 +151,74 @@ public class drive extends LinearOpMode {
             }
         }
         telemetry.update();
+    }
+    public void updating() {
+        YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
+        limelight.updateRobotOrientation(orientation.getYaw());
+        LLResult llresult = limelight.getLatestResult();
+        if(llresult != null && llresult.isValid()) {
+            Pose3D botPose = llresult.getBotpose_MT2();
+            telemetry.addData("Tx", llresult.getTx());
+            telemetry.addData("Ty", llresult.getTy());
+            telemetry.addData("Ta", llresult.getTa());
+            telemetry.addData("bot pose",botPose.toString());
+            telemetry.addData("yaw",botPose.getOrientation().getYaw(AngleUnit.DEGREES));
+        }
+        else
+        {
+            telemetry.addData("lielight","no");
+        }
+    }
+    public double getDistance() {
+        //change bot pose to flucial not mt2 turn the degree to angle
+        updating();
+        double up=75;
+        double scale= 0;
+        LLResult llresult = limelight.getLatestResult();
+        if(llresult != null && llresult.isValid()) {
+            List<LLResultTypes.FiducialResult> fiducials = llresult.getFiducialResults();
+            Pose3D botPose = llresult.getBotpose_MT2();
+            double first = (up - 27.7) / (Math.tan(20 + llresult.getTy()));
+            double second=Math.sqrt(Math.pow(botPose.getPosition().x,2)+Math.pow(botPose.getPosition().y,2));
+            scale=(first+second)/2;
+        }
+        return (scale);
+    }
+    public boolean correct(double m)
+    {
+        updating();
+        double up=75;
+        LLResult llresult = limelight.getLatestResult();
+        if(llresult != null && llresult.isValid()&&getDistance()!=0) {
+            Pose3D botPose = llresult.getBotpose_MT2();
+            double degree1,degree2,degree3,degree4,tl;
+            degree1=llresult.getTx();
+            degree2=Math.acos(botPose.getPosition().y/getDistance());
+            degree3=90+degree2;
+            tl=Math.sqrt(Math.pow(getDistance(),2)+Math.pow(m,2)-2*m*getDistance()*Math.cos(degree3));
+            degree4=Math.asin(m*Math.sin(degree3)/tl);
+            return (degree1<degree4);
+        }
+        return false;
+    }
+    public double realpower(double m)
+    {
+        //change bot pose to flucial not mt2 turn the degree to angle
+        LLResult llresult = limelight.getLatestResult();
+        Pose3D botPose = llresult.getBotpose_MT2();
+        double power= 0;
+        if(correct(m))
+        {
+            double degree1,degree2,degree3,degree4,degree5,k,realdistahce;
+            degree1=llresult.getTx();
+            degree2=Math.acos(botPose.getPosition().y/getDistance());
+            degree3=90+degree2;
+            realdistahce=Math.sqrt(Math.pow(getDistance(),2)+Math.pow(m,2)-2*m*getDistance()*Math.cos(degree3));
+            degree4=(90-degree2-degree1);
+            degree5=180-degree4;
+            k=Math.sin(degree5)*m/Math.cos(degree4);
+            power=k+realdistahce;
+        }
+        return power;
     }
 }
